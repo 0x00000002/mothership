8c245080f257468f0e5784e97073a53fc24a49de
tikonoff <tikonoff@gmail.com>
2018-01-18 12:03:09 +1300

    another attempt

--- coverageEnv/README.md
+++
@@ -1,4 +0,0 @@
-# mothership-contracts
-Mothership Token Sale Smart contracts
-
-[![Coverage Status](https://coveralls.io/repos/github/tikonoff/mothership/badge.svg?branch=master)](https://coveralls.io/github/tikonoff/mothership?branch=master)
--- truffle.js
+++ truffle.js
@@ -28,13 +28,6 @@ module.exports = {
       gas: 4000000,
       gasPrice: 20e9,
     },
-    mocha: {
-        reporter: 'eth-gas-reporter',
-        reporterOptions : {
-            currency: 'CHF',
-            gasPrice: 21
-        }
-    },
     main: {
       network_id: 1,
       provider: providerMain,
@@ -54,4 +47,11 @@ module.exports = {
       gasPrice: 20e9,
     },
   },
+  coverage: {
+      host: "localhost",
+      network_id: "*",
+      port: 8555,         // <-- If you change this, also set the port option in .solcover.js.
+      gas: 0xfffffffffff, // <-- Use this high gas value
+      gasPrice: 0x01      // <-- Use this low gas price
+  }
 }
